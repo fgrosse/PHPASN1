ce6bd34a8fe61feaa5e07519ade1a69617b3271b
Friedrich Große <friedrich.grosse@gmail.de>
2012-09-10 21:35:26 +0200

    BitString: Whitespaces are now removed from the hex input of the constructor

--- classes/asn1/ASN_BitString.class.php
+++ classes/asn1/ASN_BitString.class.php
@@ -23,16 +23,20 @@ class ASN_BitString extends ASN_Object {
     private $nrOfUnusedBits;
         
     public function __construct($value, $nrOfUnusedBits=0) {      
-        if(!is_string($value) && !is_numeric($value)) {
-            throw new Exception("ASN_BitString: unrecognized input type!");
+        if(is_string($value)) {
+            // remove gaps between hex digits
+            $value = preg_replace('/\s/','',$value);
         }
-        if(!is_numeric($nrOfUnusedBits) || $nrOfUnusedBits < 0) {
-            throw new Exception("ASN_BitString: second parameter needs to be a positive number (or zero)!");
-        }
-        
-        if(is_numeric($value)) {
+        else if(is_numeric($value)) {
             $value = dechex($value);                
         }
+        else {
+            throw new Exception("ASN_BitString: unrecognized input type!");
+        }
+        
+        if(!is_numeric($nrOfUnusedBits) || $nrOfUnusedBits < 0) {
+            throw new Exception("ASN_BitString: second parameter needs to be a positive number (or zero)!");
+        }                
         
         $this->value = $value;
         $this->nrOfUnusedBits = $nrOfUnusedBits;
@@ -48,12 +52,12 @@ class ASN_BitString extends ASN_Object {
             // transform values like 1F2 to 01F2
             $value = "0".$value;
         } 
-        $length = 1 + ceil((strlen($value)/2));
-        return $length;
+        // add one to the length for the first octet which encodes the number of unused bits in the last octet
+        return strlen($value)/2 + 1;
     }
     
     protected function getEncodedValue() {
-        $value = $this->value;               
+        $value = $this->value;
         
         if(strlen($value) %2 != 0) {
             // transform values like 1F2 to 01F2
@@ -68,8 +72,7 @@ class ASN_BitString extends ASN_Object {
             // get the hex value byte by byte from the string and and add it to binary result
             $result .= chr(hexdec(substr($value,0,2)));
             $value = substr($value,2);
-        }
-        
+        }        
         return $result;
     }
            
