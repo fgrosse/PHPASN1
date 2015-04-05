1966cc2ab49f56ca577d20f6433543d5ae9ab25d
Friedrich Große <friedrich.grosse@classmarkets.com>
2015-04-05 10:16:03 +0200

    Switch to MIT license

--- examples/Issue14.php
+++ examples/Issue14.php
@@ -1,21 +1,11 @@
 <?php
 /*
- * This file is part of PHPASN1 written by Friedrich Große.
+ * This file is part of the PHPASN1 library.
  *
- * Copyright © Friedrich Große, Berlin 2013
+ * Copyright © Friedrich Große <friedrich.grosse@gmail.com>
  *
- * PHPASN1 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PHPASN1 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PHPASN1.  If not, see <http://www.gnu.org/licenses/>.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 require_once __DIR__.'/../vendor/autoload.php';
--- tests/DocumentationExamplesTest.php
+++ tests/DocumentationExamplesTest.php
@@ -4,18 +4,8 @@
  *
  * Copyright © Friedrich Große, Berlin 2012
  *
- * PHPASN1 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PHPASN1 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PHPASN1.  If not, see <http://www.gnu.org/licenses/>.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace FG\Test;
@@ -63,6 +53,6 @@ class DocumentationExamplesTest extends ASN1TestCase
         $binaryData = base64_decode($base64String);
 
         $asnObject = Object::fromBinary($binaryData);
-        $this->assertInstanceOf("\FG\ASN1\Universal\Sequence", $asnObject);
+        $this->assertInstanceOf('\FG\ASN1\Universal\Sequence', $asnObject);
     }
 }
