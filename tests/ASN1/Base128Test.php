47def04241adb0288026fe2c508f2eace3e1f4c2
Friedrich Große <friedrich.grosse@gmail.com>
2015-05-09 10:33:31 +0200

    Fix tests on 64bit systems

--- tests/ASN1/Base128Test.php
+++ tests/ASN1/Base128Test.php
@@ -96,12 +96,16 @@ class Base128Test extends \PHPUnit_Framework_TestCase
         Base128::decode("\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x7F");
     }
 
-    /**
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Value (0xDEADBEEF) exceeds the maximum integer length when base128-encoded.
-     */
     public function testEncodeFailsIfOverflows()
     {
-        Base128::encode(0xDEADBEEF);
+        $value = PHP_INT_MAX+1;
+        try {
+            Base128::encode($value);
+            $this->fail('Failed to throw an exception');
+        } catch (\InvalidArgumentException $exception) {
+            $expected = sprintf('Value (0x%s) exceeds the maximum integer length when base128-encoded.', strtoupper(dechex($value)));
+            $this->assertEquals($expected, $exception->getMessage());
+        }
+
     }
 }
