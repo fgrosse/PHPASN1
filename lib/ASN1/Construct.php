332c8532d81130daeaabacfe92a632721173f00b
Friedrich Große <friedrich.grosse@gmail.com>
2015-08-17 20:20:14 +0200

    Remove obsolete comments
    These comments are inherited from the interface they implement anyways

--- lib/ASN1/Construct.php
+++ lib/ASN1/Construct.php
@@ -10,7 +10,9 @@
 
 namespace FG\ASN1;
 
-abstract class Construct extends Object implements \Iterator, Parsable
+use Iterator;
+
+abstract class Construct extends Object implements Iterator, Parsable
 {
     /** @var Object[] */
     protected $children;
@@ -28,41 +30,26 @@ abstract class Construct extends Object implements \Iterator, Parsable
         return $this->children;
     }
 
-    /**
-     * Rewind the Iterator to the first element (Iterator::rewind)
-     */
     public function rewind()
     {
         $this->iteratorPosition = 0;
     }
 
-    /**
-     * Return the current element (Iterator::current)
-     */
     public function current()
     {
         return $this->children[$this->iteratorPosition];
     }
 
-    /**
-     * Return the key of the current element (Iterator::key)
-     */
     public function key()
     {
         return $this->iteratorPosition;
     }
 
-    /**
-     * Move forward to next element (Iterator::next)
-     */
     public function next()
     {
         $this->iteratorPosition++;
     }
 
-    /**
-     * Checks if current position is valid (Iterator::valid)
-     */
     public function valid()
     {
         return isset($this->children[$this->iteratorPosition]);
