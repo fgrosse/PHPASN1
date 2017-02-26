1c57956688d6307f338412f2d12b29b0cb29e221
Friedrich Große <friedrich.grosse@gmail.com>
2017-02-26 13:42:21 +0100

    Use mbstring functions only if extension is loaded

--- lib/_mbstring_support.php
+++ lib/_mbstring_support.php
@@ -2,10 +2,18 @@
 
 function safeStrlen($string)
 {
+    if (extension_loaded('mbstring')) {
+        return mb_strlen($string, '8bit');
+    }
+
     return strlen($string);
 }
 
 function safeSubstr($string, $start, $length = 2147483647)
 {
+    if (extension_loaded('mbstring')) {
+        mb_substr($string, $start, $length, '8bit');
+    }
+
     return substr($string, $start, $length);
 }
