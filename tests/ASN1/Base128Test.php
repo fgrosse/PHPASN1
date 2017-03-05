619303ddaba7ebd8129028ccbf855aa6f8230fdc
Friedrich Große <friedrich.grosse@gmail.com>
2017-03-05 10:14:17 +0100

    Update phpunit

--- tests/ASN1/Base128Test.php
+++ tests/ASN1/Base128Test.php
@@ -3,8 +3,9 @@
 namespace FG\tests\ASN1;
 
 use FG\ASN1\Base128;
+use PHPUnit\Framework\TestCase;
 
-class Base128Test extends \PHPUnit_Framework_TestCase
+class Base128Test extends TestCase
 {
     /**
      * @dataProvider getDataToDecode
