9d33d38127aba2aa29f991a78dadb3092ebfcc9f
Nathan Perkins <nathan@maxmo.net>
2018-03-10 14:01:29 -0500

    Bugfix in GMP implementation

--- lib/Utility/BigIntegerGmp.php
+++ lib/Utility/BigIntegerGmp.php
@@ -45,7 +45,7 @@ class BigIntegerGmp extends BigInteger
     public function toInteger()
     {
         if ($this->compare(PHP_INT_MAX) > 0 || $this->compare(PHP_INT_MIN) < 0) {
-            throw new \OverflowException(sprintf('Can not represent %s as integer.', $this->_str));
+            throw new \OverflowException(sprintf('Can not represent %s as integer.', $this));
         }
         return gmp_intval($this->_rh);
     }
